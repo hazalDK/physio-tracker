7b5ca5aad2f92defb11ea57ac52f33a740082fa4
Hazal Deniz Kara <hazalkara1212@gmail.com>
2025-01-21 14:53:54 +0000

    Updated models and gitignore

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Exercises, UserExercise
 
-# Register your models here.
+# Makes sure we can see the extra added fields in admin portal
+class CustomUserAdmin(UserAdmin):
+    model = User
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email', 'full_name', 'date_of_birth', 'exercises')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'full_name', 'date_of_birth', 'exercises'),
+        }),
+    )
+    list_display = ('username', 'email', 'full_name', 'date_of_birth', 'is_staff')
+    search_fields = ('username', 'email', 'full_name')
+    ordering = ('username',)
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Exercises)
+admin.site.register(UserExercise)
--- api/models.py
+++ api/models.py
@@ -5,17 +5,17 @@ from django.contrib.auth.models import AbstractUser
 
 # Exercise model
 # GET: get a list of all exercises in database
-# POST: not needed - hobby additions shouldn't be done by client users (maybe)
-# PUT: not needed - client users cannot edit the disc (for now?)
-# DELETE: also not needed for the client user
+# POST: users can add their own exercises to the database
+# PUT: users can edit or update the exercises in the database
 class Exercises(models.Model):
     '''
-    Hobby model with str and as_dict function.
+    Exercise model with str and as_dict function, including video link.
     '''
-    name = models.CharField(default="Hobby", max_length=100, unique=True)
+    name = models.CharField(default="Exercise", max_length=100, unique=True)
     description = models.TextField(default="Description", max_length=500)
     reps = models.IntegerField(default=0)
     sets = models.IntegerField(default=0)
+    video_link = models.URLField(default="", blank=True, max_length=500)  # New field for video links
 
     def __str__(self):
         return self.name
@@ -27,18 +27,36 @@ class Exercises(models.Model):
             'description': self.description,
             "reps": self.reps,
             "sets": self.sets,
+            "video_link": self.video_link,
         }
 
 
-# Custom user model
-# GET: two variants: get another user's profile (viewing other profiles)
-#                    get current user's details (viewing their profile etc)
-# POST: signup - no access via views should be available
-# PUT: edit or update details about the user - name(?), dob, hobby list
-# DELETE: delete user in profile (only for current user)
-# BONUS GET: search for users with a particular hobby(s) and output - TODO
-#   this leads to the profile page GET (1st one)
+# User Exercise model
+# GET: get a list of all exercises the current user has
+# POST: users can add exercises to their list
+# PUT: users can edit or update the amount of exercises they have
+# DELETE: users can remove assigned exercises from themselves.
+class UserExercise(models.Model):
+    '''
+    Through table for storing user-specific exercise details.
+    '''
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    exercise = models.ForeignKey(Exercises, on_delete=models.CASCADE)
+    sets = models.IntegerField(default=0)
+    reps = models.IntegerField(default=0)
 
+    class Meta:
+        unique_together = ('user', 'exercise')
+
+    def __str__(self):
+        return f"{self.user.full_name} - {self.exercise.name} (Sets: {self.sets}, Reps: {self.reps})"
+
+# Custom user model
+# GET: two variants: get physiotherapist user's details (viewing their profile etc)
+#                    get patient user's details (viewing their profile etc)
+# POST: signup - no access via views should be available
+# PUT: edit or update details about the user - name, dob, exercise list
+# DELETE: delete user in profile (only for current user)
 class User(AbstractUser):
     '''
     User account model with str and as_dict function.
@@ -60,7 +78,7 @@ class User(AbstractUser):
             'full_name': self.full_name,
             'email': self.email,
             'date_of_birth': self.date_of_birth,
-            'hobbies': list(self.exercises.values('id', 'name')),
+            'exercises': list(self.exercises.values('id', 'name')),
         }
     
     def priv_as_dict(self):
@@ -69,7 +87,7 @@ class User(AbstractUser):
             'username': self.username,
             'first_name': self.first_name,
             'date_of_birth': self.date_of_birth,
-            'hobbies': list(self.exercises.values('id', 'name')),
+            'exercises': list(self.exercises.values('id', 'name')),
         }
     
     def save(self, *args, **kwargs):
