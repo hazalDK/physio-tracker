9ae71845c5e59f92f1530d46a56c7ad6ebb42d37
Hazal Deniz Kara <hazalkara1212@gmail.com>
2025-05-05 03:04:40 +0100

    got rid of dist mention in settings.py

--- project/settings.py
+++ project/settings.py
@@ -101,7 +101,6 @@ AUTH_USER_MODEL = 'api.User'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'frontend/dist')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -169,10 +168,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/dist/')
-]
 
 INTERNAL_IPS = ['127.0.0.1']
 
